4352b5e579c78730fa410734592370f6c629a8b7
Kyuri <kyuri.reddy@gmail.com>
2026-04-17 13:31:29 +0200

    Fixed column indexing and best student tracking

--- getbest.py
+++ getbest.py
@@ -5,23 +5,39 @@ import sys
 def getCols(f):
     ''' Identify the columns that contain the marks and student numbers '''
     headings = f.readline().strip().split(",")
-    i=1
+    i = 0
+    num_col = -1
+    mark_col = -1
+
     for head in headings:
-        if head == "Student Number": num_col=i
-        elif head == "Mark" : mark_col = i
+        if head == "Student Number":
+            num_col = i
+        elif head == "Mark":
+            mark_col = i
+        i += 1
+
     return (num_col, mark_col)
 
-def findTop(f,num_col, mark_col):
+
+def findTop(f, num_col, mark_col):
     ''' finds the top student in the class '''
-    best = best_idx =  0
+    best = -1
+    best_idx = None
+
     for line in f:
         data = line.strip().split(",")
         mark = int(data[mark_col])
+        student_num = data[num_col]  
+
         if mark > best:
-            best=mark
+            best = mark
+            best_idx = student_num   
+
     return best_idx, best
 
-f = open(sys.argv[1])
-num_col, mark_col = getCols(f)
-best_idx, best = findTop(f,num_col,mark_col)
-print("The top student was student %s with %d"%(best_idx,best))
+
+if __name__ == "__main__":
+    f = open(sys.argv[1])
+    num_col, mark_col = getCols(f)
+    best_idx, best = findTop(f, num_col, mark_col)
+    print("The top student was student %s with %d" % (best_idx, best))
